a55c65fcbddfa2d3748eae9294c17b142aca4db3
Pinapelz <yukais@pinapelz.com>
2026-01-23 22:37:07 -0800

    change spaghetti to require 1 gold

--- src/main/java/com/pinapelz/TRWHytale.java
+++ src/main/java/com/pinapelz/TRWHytale.java
@@ -6,6 +6,7 @@ import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import com.hypixel.hytale.server.core.universe.world.events.AllWorldsLoadedEvent;
 import com.pinapelz.commands.AppearTRW;
 import com.pinapelz.commands.PingTRW;
+import com.pinapelz.events.CraftRecipeEventSystem;
 import com.pinapelz.events.ShutdownEvent;
 import com.pinapelz.events.StartUpEvent;
 
@@ -13,20 +14,24 @@ import javax.annotation.Nonnull;
 
 public class TRWHytale extends JavaPlugin {
 
+
     public TRWHytale(@Nonnull JavaPluginInit init) {
         super(init);
     }
 
     @Override
-    protected void setup() {
+    public void setup() {
         this.getCommandRegistry().registerCommand(new PingTRW("trw", "TRW Version and Info"));
         this.getCommandRegistry().registerCommand(new AppearTRW("trw-appear", "Send a jumpscare to someone"));
         getEventRegistry().register(
                 AllWorldsLoadedEvent.class,
                 StartUpEvent::onServerReady
         );
-        PacketAdapters.registerInbound(new InteractionPacketListener());
+        getEntityStoreRegistry().registerSystem(new CraftRecipeEventSystem());
+        PacketAdapters.registerInbound(new PacketListener());
+
     }
+
 
     @Override
     public void shutdown() {
